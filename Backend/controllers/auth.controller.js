dbdfafba7bd692bc89cf26d71030a06ad048c821
Nikhil Sharma <nikhils.sharma2409@gmail.com>
2026-01-14 22:11:48 +0530

    cookies changes

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -77,8 +77,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // 🔥 REQUIRED
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // 🔥 REQUIRED
+      secure: true, // 🔥 REQUIRED
+      sameSite: none,
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
